6aff5c65174865318f24595dad79f4a97232045e
Yusuke Mitome <info@mitomex.jp>
2021-01-29 07:26:56 +0900

    init

---
+++ src/User.ts
@@ -0,0 +1,17 @@
+import faker from 'faker';
+
+class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.name.firstName();
+    // 上記にある location は TypeScript に型を入れていくと伝えているのみで
+    // 実際に値があるわけではないので以下のようにすると this.location が undefined で
+    // .lat に値を代入できない。
+    // this.location.lat = faker.address.latitude(); 
+  }
+}
